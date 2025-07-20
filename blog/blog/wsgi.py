fd23ed8743b0e281ff8ed0e837793772dc1aa7aa
Faizan-Khambre <163112231+Faizan-Khambre@users.noreply.github.com>
2025-07-20 20:48:52 +0530

    Updated Procfile and wsgi.py file

--- blog/blog/wsgi.py
+++ blog/blog/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog.settings')
+
+# Initialize Django
+django.setup()
+
+# Run migrations automatically during WSGI startup
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
 
 application = get_wsgi_application()
